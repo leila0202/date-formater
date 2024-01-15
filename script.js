985e17caf41540572c219ad37145efa08507643b
Leila Zrim <leilazrim@gmail.com>
2024-01-15 15:31:28 +0100

    [js - 04] [Step 24] Add default case. This finishes this project

--- script.js
+++ script.js
@@ -23,5 +23,7 @@ dateOptionsSelectElement.addEventListener("change", () => {
     case "mm-dd-yyyy-h-mm":
       currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
       break;
+    default:
+      currentDateParagraph.textContent = formattedDate;
   }
 });
