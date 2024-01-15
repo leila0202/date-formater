a0e36547fce701d1c7e943332e2da9e4150dd012
Leila Zrim <leilazrim@gmail.com>
2024-01-15 15:02:49 +0100

    [js - 04] [Step 05] Get the current month

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
+
 const date = new Date();
 const day = date.getDate();
+// "getMonth" is zero based so "+1" to get the correct month
+const month = date.getMonth() + 1;
