4c39921a018bb36984ffb23173214a15c69c5f95
Leila Zrim <leilazrim@gmail.com>
2024-01-15 15:04:02 +0100

    [js - 04] [Step 06] Get current year

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const date = new Date();
 const day = date.getDate();
 // "getMonth" is zero based so "+1" to get the correct month
 const month = date.getMonth() + 1;
+const year = date.getFullYear();
