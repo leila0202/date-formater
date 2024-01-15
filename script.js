ddc1bc065b92316e189161bb7582cf1d37478a63
Leila Zrim <leilazrim@gmail.com>
2024-01-15 15:17:49 +0100

    [js - 04] [Step 17] Starting to implement the first case

--- script.js
+++ script.js
@@ -14,5 +14,7 @@ currentDateParagraph.textContent = formattedDate;
 
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate;
   }
 });
