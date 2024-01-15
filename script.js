2f420c2083864ae41f6c1f1cd657a577ff266046
Leila Zrim <leilazrim@gmail.com>
2024-01-15 15:01:31 +0100

    [js - 04] [Step 04] Get the current day

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 const date = new Date();
+const day = date.getDate();
